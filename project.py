fc5f268bd8fe96ecd9a7c41a1e9ded13ee060e5b
mattinasa <2959048650@qq.com>
2026-03-28 17:24:46 +0800

    第一次调试前

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ class Student:
         self.college=college
 
     def __str__(self):#用于友好输出，不然就会输出我们看不懂的东西
-        return f"姓名：{self.name} 班级：{self.classNum} 学号：{self.id} 性别：{self.gender} 学院：{self.college}"
+        return f"姓名：{self.name} 班级：{self.classNum} 性别：{self.gender} 学院：{self.college}"#索引用学号，就不需要输出学号了
 
 class ExamSystem:
     def __init__(self,filename):
@@ -16,25 +16,34 @@ class ExamSystem:
         self.loading()
 
     def loading(self):
+
         try:
             with open(self.filename,'r') as f:#尝试打开文件
                 lines = f.readlines()#读取学生信息
 
                 for i in range(1,len(lines)):#枚举每一个学生
+                    name=lines[i][1]
+                    gender=lines[i][2]
+                    classNum=lines[i][3]
+                    id=lines[i][4]
+                    college=lines[i][5]
+                    self.students[id] = Student(name,classNum,id,gender,college)#以学号为索引来输出吧
 
-                    student=[]#存储除了学号以外的信息，用于value
-
-                    for j in range(6):#枚举学生的每一个信息，然后除了学号以外存储在一个列表里，当作value
-                        if j!=4:#除了学号
-                            student.append(lines[i][j].strip())
-
-                    self.students[lines[i][4].strip()] = student
+                    #调试
+                    print(self.students[id].__str__())
 
 
         except FileNotFoundError:
-            print("File not found")
+            print("File not found")#比较友好地提示文件没有找到
             self.students = {}
 
     @staticmethod
     def validate(student_id):#一个简单的id基础格式校验，要求就是纯数字
-        return student_id.isdigit()
+        return student_id.isdigit()#判断是不是纯数字
+
+#这是个调试
+try:
+    system = ExamSystem("人工智能编程语言学生名单.txt")
+    system.loading()
+except FileNotFoundError:
+    print("File not found")
